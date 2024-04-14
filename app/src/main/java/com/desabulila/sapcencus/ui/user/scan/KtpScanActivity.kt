f653fd6ddb9be353a0c05f0469a63d0f48619ad8
Bimantoro <muhbimantoro08@gmail.com>
2024-04-14 20:09:29 +0800

    init commit

---
+++ app/src/main/java/com/desabulila/sapcencus/ui/user/scan/KtpScanActivity.kt
@@ -0,0 +1,21 @@
+package com.desabulila.sapcencus.ui.user.scan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.desabulila.sapcencus.R
+
+class KtpScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ktp_scan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
